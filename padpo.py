0baabab98cc9ebbf05e3b1f9daeddc93802da74d
Vincent Poulailleau <vpoulailleau@gmail.com>
2019-11-18 09:07:11 +0100

    add abbr

--- padpo.py
+++ padpo.py
@@ -68,6 +68,7 @@ class PoItem:
         text = re.sub(r"``(.*?)``", r"« \1 »", text)
         text = re.sub(r":pep:`(.*?)`", r"PEP \1", text)
         for term in (
+            "abbr",
             "attr",
             "class",
             "const",
