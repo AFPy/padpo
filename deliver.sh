0c4dde9555b11b9a57ad2c4ad9c792cd883b9069
Vincent Poulailleau <vpoulailleau@gmail.com>
2019-11-18 17:41:48 +0100

    add delivery script

---
+++ deliver.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+cat padpo/__init__.py | grep version
+cat setup.py | grep version
+echo "ready ?"
+read BOOL
+
+git pull
+source venv/bin/activate
+
+# clean
+rm -rf build/ dist/ .eggs/
+find . -name '*.egg-info' -exec rm -fr {} +
+find . -name '*.egg' -exec rm -fr {} +
+find . -name '*.pyc' -exec rm -f {} +
+find . -name '*.pyo' -exec rm -f {} +
+find . -name '*~' -exec rm -f {} +
+find . -name '__pycache__' -exec rm -fr {} +
+rm -fr .tox/
+rm -f .coverage
+rm -fr htmlcov/
+rm -fr .pytest_cache
+
+
+# package creation
+python setup.py sdist
+python setup.py bdist_wheel
+twine upload dist/*
