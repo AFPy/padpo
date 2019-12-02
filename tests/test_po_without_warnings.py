1a4535c3e7684656c639e29e22d009b3b50f1913
Vincent Poulailleau <vpoulailleau@gmail.com>
2019-12-02 18:32:53 +0100

    add test against manually verified po files

--- tests/test_po_without_warnings.py
+++ tests/test_po_without_warnings.py
@@ -1,9 +1,18 @@
+from pathlib import Path
+
 from padpo.padpo import check_file
 
 
-def test_hello():
-    assert True != False
+def pytest_generate_tests(metafunc):
+    if "known_good_po_file" in metafunc.fixturenames:
+        # assume using tox (that cd into tests directory)
+        directory = Path("./po_without_warnings")
+        files = directory.rglob("*.po")
+        metafunc.parametrize(
+            "known_good_po_file", [str(file) for file in files]
+        )
 
 
-def test_coverage():
-    check_file("/home/vincent/Documents/programmation/padpo/functions.po")
+def test_coverage(known_good_po_file):
+    # TODO get an empty list of warnings
+    assert check_file(known_good_po_file) == False
