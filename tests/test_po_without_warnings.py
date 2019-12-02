34fb968ded0b12c4f2a8e9252af6c90e17737228
Vincent Poulailleau <vpoulailleau@gmail.com>
2019-12-02 19:03:38 +0100

    apply linter remarks

--- tests/test_po_without_warnings.py
+++ tests/test_po_without_warnings.py
@@ -1,9 +1,11 @@
+"""Test known to be good files."""
 from pathlib import Path
 
 from padpo.padpo import check_file
 
 
 def pytest_generate_tests(metafunc):
+    """Parametrize tests according to po_without_warnings directory content."""
     if "known_good_po_file" in metafunc.fixturenames:
         # assume using tox (that cd into tests directory)
         directory = Path("./po_without_warnings")
@@ -13,7 +15,8 @@ def pytest_generate_tests(metafunc):
         )
 
 
-def test_coverage(known_good_po_file):
+def test_no_error_no_warning(known_good_po_file):
+    """Test known to be good files."""
     errors, warnings = check_file(known_good_po_file)
     assert not errors
     assert not warnings
