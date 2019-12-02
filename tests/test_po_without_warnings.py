2a598c54e014eeacc4fe41e1e223f59f3664cbd6
Vincent Poulailleau <vpoulailleau@gmail.com>
2019-12-02 18:17:59 +0100

    use tox to run tests

---
+++ tests/test_po_without_warnings.py
@@ -0,0 +1,9 @@
+from padpo.padpo import check_file
+
+
+def test_hello():
+    assert True != False
+
+
+def test_coverage():
+    check_file("/home/vincent/Documents/programmation/padpo/functions.po")
