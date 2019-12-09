ea5f7a3f60e1f7cbd611af6c74b997af3b927a8f
Vincent Poulailleau <vpoulailleau@gmail.com>
2019-12-09 12:41:41 +0100

    add glossary checker

--- padpo/checkers/__init__.py
+++ padpo/checkers/__init__.py
@@ -2,6 +2,7 @@
 from padpo.checkers.doublespace import DoubleSpaceChecker
 from padpo.checkers.empty import EmptyChecker
 from padpo.checkers.fuzzy import FuzzyChecker
+from padpo.checkers.glossary import GlossaryChecker
 from padpo.checkers.grammalecte import GrammalecteChecker
 from padpo.checkers.linelength import LineLengthChecker
 from padpo.checkers.nbsp import NonBreakableSpaceChecker
@@ -11,6 +12,7 @@ checkers = [
     EmptyChecker(),
     FuzzyChecker(),
     GrammalecteChecker(),
+    GlossaryChecker(),
     LineLengthChecker(),
     NonBreakableSpaceChecker(),
 ]
