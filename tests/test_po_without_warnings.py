14aa67a939608a7d4b43b099ff9eb407b8e98827
Vincent Poulailleau <vpoulailleau@gmail.com>
2020-09-07 09:00:18 +0200

    test from project root dir

--- tests/test_po_without_warnings.py
+++ tests/test_po_without_warnings.py
@@ -8,10 +8,10 @@ def pytest_generate_tests(metafunc):
     """Parametrize tests according to po_without_warnings directory content."""
     if "known_good_po_file" in metafunc.fixturenames:
         # assume using tox (that cd into tests directory)
-        directory = Path("./po_without_warnings")
-        files = directory.rglob("*.po")
+        files = list(Path("./po_without_warnings").rglob("*.po"))
+        files.extend(Path("./tests/po_without_warnings").rglob("*.po"))
         metafunc.parametrize(
-            "known_good_po_file", [str(file) for file in files]
+            "known_good_po_file", [str(file_path) for file_path in files]
         )
 
 
