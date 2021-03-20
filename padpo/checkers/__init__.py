b944641e503bb94f0d3e4c4f4b6e77bfaa01600d
Bruno Geninatti <brunogeninatti@gmail.com>
2021-03-20 16:47:06 +0000

    disable NBSP checker

--- padpo/checkers/__init__.py
+++ padpo/checkers/__init__.py
@@ -3,12 +3,10 @@ from padpo.checkers.empty import EmptyChecker
 from padpo.checkers.fuzzy import FuzzyChecker
 from padpo.checkers.glossary import GlossaryChecker
 from padpo.checkers.linelength import LineLengthChecker
-from padpo.checkers.nbsp import NonBreakableSpaceChecker
 
 checkers = [
     EmptyChecker(),
     FuzzyChecker(),
     GlossaryChecker(),
     LineLengthChecker(),
-    NonBreakableSpaceChecker(),
 ]
